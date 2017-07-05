17c8af2c4fdf2d06e75d000a1ca2c4fd7868796e
Anerão Junior <anerao.junior@gmail.com>
2017-07-05 12:05:35 -0500

    Adicionando validação de CNPJ

--- classes/Validacao.class.php
+++ classes/Validacao.class.php
@@ -4,6 +4,7 @@ class Validacao
 {
     private $cpf;
     private $email;
+    private $cnpj;
 
     public function setCPF($cpf)
     {
@@ -68,6 +69,34 @@ class Validacao
 
             return true;
         }
+    }
+
+    public function validarCNPJ($cnpj)
+    {
+        $cnpj = preg_replace('/[^0-9]/', '', (string) $cnpj);
+
+        if (strlen($cnpj) != 14)
+            return false;
+
+        // Validar primeiro dígito verificador
+        for ($i = 0, $j = 5, $soma = 0; $i < 12; $i++)
+        {
+            $soma += $cnpj{$i} * $j;
+            $j = ($j == 2) ? 9 : $j - 1;
+        }
+        $resto = $soma % 11;
+        if ($cnpj{12} != ($resto < 2 ? 0 : 11 - $resto))
+            return false;
+
+        // Validar segundo dígito verificador
+        for ($i = 0, $j = 6, $soma = 0; $i < 13; $i++)
+        {
+            $soma += $cnpj{$i} * $j;
+            $j = ($j == 2) ? 9 : $j - 1;
+        }
+        $resto = $soma % 11;
+
+        return $cnpj{13} == ($resto < 2 ? 0 : 11 - $resto);
     }
 
     public function validarEmail($email)
