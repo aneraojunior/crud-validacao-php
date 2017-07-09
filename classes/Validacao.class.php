16b9d65bb7327339063b61e3386ded6e8cd28278
Anerão Junior <anerao.junior@gmail.com>
2017-07-09 15:54:42 -0500

    Adicionando validação de CEP e retornando: - logradouro; - complemento; - bairro; - localidade; - uf; - unidade; - ibge; - gia;
    Obrigado especial ao site ViaCEP por fornecer a API

--- classes/Validacao.class.php
+++ classes/Validacao.class.php
@@ -101,4 +101,23 @@ class Validacao
             }
         }
     }
+
+    public function validarCEP($cep)
+    {
+        $cep = preg_replace("/[^0-9]/", "", $cep);
+
+        $link = "https://viacep.com.br/ws/$cep/json/unicode/";
+
+        $ch = curl_init($link);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+        $response = curl_exec($ch);
+
+        curl_close($ch);
+
+        $data = json_decode($response, true);
+
+        return $data;
+    }
 }
